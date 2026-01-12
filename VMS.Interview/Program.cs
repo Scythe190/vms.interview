a367f158b4c6b4d682e7a010fc01f761540d2e5d
Abdulaziz Hachem <A.Hachem@diyarme.com>
2026-01-12 22:02:32 +0300

    Add project files.

---
+++ VMS.Interview/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.HttpOverrides;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+    options.KnownNetworks.Clear();
+    options.KnownProxies.Clear();
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseForwardedHeaders();
+
+var useHttpsRedirection = builder.Configuration.GetValue("UseHttpsRedirection", app.Environment.IsDevelopment());
+if (useHttpsRedirection)
+{
+    app.UseHttpsRedirection();
+}
+
+app.UseAuthorization();
+
+app.MapGet("/health", () => Results.Ok("ok"));
+
+app.MapControllers();
+
+app.Run();
